e01d8612a03aeb50af765a5965c8d5111a3fc57d
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-11-06 17:51:18 +0000

    varible is done

---
+++ 01_bascis/01_Variable.js
@@ -0,0 +1,6 @@
+const accountId=123344;
+let accountEmail="saurabh@gmail.com";
+var accountPassword="12321";
+accountCity="jaipur";
+// we always try to use let instead of var because it is block scope not
+console.table([accountId,accountEmail,accountPassword,accountCity]);
